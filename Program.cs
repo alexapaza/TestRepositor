59906cb3f6023b8d8df3b5f5a5eeee1554cd4b9d
alexapaza08 <alex.apaza7070@gmail.com>
2019-10-04 08:21:39 -0400

    Practice 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace FirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int number in generadorFibonnaci(10))
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+        public static List<int> generadorFibonnaci(int limit){
+            List<int> fibonacci = new List<int>();
+            fibonacci.Add(0);
+            fibonacci.Add(1);
+            for (int i = 1; i < limit;  i++ ){
+                int auxiliarVar=fibonacci[i-1] ;
+                int auxiliarVar2=fibonacci[i];
+                int auxiliarVar3=auxiliarVar + auxiliarVar2;                
+                fibonacci.Add(auxiliarVar3);               
+            }
+
+            return fibonacci ; 
+            
+        } 
+
+    }
+}
