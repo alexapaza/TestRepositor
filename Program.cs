628474ef23ff4b0d553e0dc6f638c01027bfdfe6
alexapaza08 <alex.apaza7070@gmail.com>
2019-10-06 23:19:04 -0400

    to check with Juan

--- Program.cs
+++ Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 namespace FirstClass
 {
     class Program
-    {
+    {   
+        
         static void Main(string[] args)
         {
-            foreach (int number in generadorFibonnaci(10))
+           /*  foreach (int number in generadorFibonnaci(10))
             {
                 Console.WriteLine(number);
             }
+            */
+
+
+              IEnumerable<int> mysequence = GeneratorSequence();
+            /*Console.WriteLine("press key");
+            Console.ReadKey();  */
+            int i = 0;
+            foreach (var item in mysequence)
+            {
+                if(i==10){
+                   break;     
+                }
+                Console.WriteLine(item);
+                i++;
+            }
+
 
         }
         public static List<int> generadorFibonnaci(int limit){
@@ -26,7 +43,46 @@ namespace FirstClass
             return fibonacci ; 
             
         } 
-        public IEnumerable<int>  
+        public static IEnumerable<int> GeneratorSequence(){
+            int value1 = 0;
+            Console.WriteLine("first line");
+            yield return 10;    
+
+            Console.WriteLine("second line");
+            yield return 20;    
+
+            while(value1 < 2){
+                Console.WriteLine("abase line");
+                yield return 1000;        
+                value1++;
+
+                
+                Console.WriteLine("local var:"+value1);
+                Console.WriteLine("bbase line");
+                yield return 2000;     
+                value1++;   
+                Console.WriteLine("local var:"+value1);
+                Console.WriteLine("cbase line");
+                yield return 3000;     
+                value1++;
+                Console.WriteLine("local var:"+value1);
+                Console.WriteLine("dbase line");
+                yield return 4000;     
+                value1++;
+                Console.WriteLine("local var:"+value1);
+                Console.WriteLine("ebase line");
+                yield return 5000;     
+                value1++;
+            }
+
+
+            Console.WriteLine("third line");
+            yield return 30;    
+            Console.WriteLine("forth line");
+            yield return 40;    
+
+
+        }
 
     }
 }
